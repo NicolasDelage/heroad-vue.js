5ad94a16e3ee5a9e26a5aabac7eb1837a8ecf59b
nicolasdelage <nicolas.delage@whereyoulove.com>
2020-12-19 14:07:28 +0100

    feat: setup app and authentication user

---
+++ app/src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{runs}}</div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      runs: null
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:8000/api/heroad/run/").then(response  => {
+      this.runs = response
+      console.log(response)
+    });
+  }
+};
+</script>
+
+<style scoped></style>
